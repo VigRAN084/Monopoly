fc0d2d5de7ed7f9383f28ee890139a979dc5df74
Vignesh Rangarajan <vignesh@vigneshs-air.lan>
2021-05-08 21:48:06 -0700

    wip - but fixed toggling players, displaying current options, and taking user feedback

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -1,9 +1,47 @@
 public class Monopoly {
-    public static void playGame() {
+    private Player p1;
+    private Player p2;
+
+    public void playGame() {
         Board b = new Board();
+        addPlayers();
+        boolean gameNotWon = false;
+        Player currPlayer = p1;
+        while (!gameNotWon) {
+            currPlayer.playTurn();
+            currPlayer = togglePlayer();
+        }
         b.printBoard();
     }
-    public static void main(String[] args){
-        Monopoly.playGame();
+
+    private void addPlayers() {
+        p1 = new Player();
+        p1.setMoney(1500);
+        p1.setPiece("A");
+        p1.setSpaceCurrentlyOn(1);
+        p1.setMyTurn(true);
+
+        p2 = new Player();
+        p2.setMoney(1500);
+        p2.setPiece("B");
+        p2.setSpaceCurrentlyOn(1);
     }
+
+    private Player togglePlayer() {
+        if (p1.isMyTurn()){
+            p1.setMyTurn(false);
+            p2.setMyTurn(true);
+            return p2;
+        } else {
+            p1.setMyTurn(true);
+            p2.setMyTurn(false);
+            return p1;
+        }
+    }
+
+    public static void main(String[] args){
+        Monopoly m = new Monopoly();
+        m.playGame();
+    }
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,76 @@
+import java.util.Scanner;
+
 public class Player {
+    public static String[] PLAY_OPTIONS = {
+            "1: Roll",
+            "2: Buy Houses or Hotels",
+            "3: Trade with another Player",
+            "4: Unmortgage",
+            "5: Quit Playing Game"
+    };
+    private String piece;
+    private int money;
+    private int spaceCurrentlyOn = 1;
+    private boolean myTurn = false;
+
+    public Player() {}
+
+    public String getPiece() {
+        return piece;
+    }
+
+    public void setPiece(String piece) {
+        this.piece = piece;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getSpaceCurrentlyOn() {
+        return spaceCurrentlyOn;
+    }
+
+    public void setSpaceCurrentlyOn(int spaceCurrentlyOn) {
+        this.spaceCurrentlyOn = spaceCurrentlyOn;
+    }
+
+    public boolean isMyTurn() {
+        return myTurn;
+    }
+
+    public void setMyTurn(boolean myTurn) {
+        this.myTurn = myTurn;
+    }
+
+    public void playTurn() {
+        if (!this.isMyTurn()) return;
+        System.out.println("Player " + piece + "'s turn");
+        int option = promptOption();
+    }
+
+    private int promptOption() {
+        System.out.println("Your options are: ");
+        for (String s: PLAY_OPTIONS){
+            System.out.println(s);
+        }
+        boolean validInput = false;
+        int optionNum = -1;
+        while(!validInput) {
+            System.out.println("Please enter a number between 1 and 5, inclusive");
+            Scanner scanner = new Scanner(System.in);
+            optionNum = scanner.nextInt();
+            if (optionNum >= 1 && optionNum <= 5){
+                validInput = true;
+            }
+            else{
+                System.out.println("Invalid input");
+            }
+        }
+        return optionNum;
+    }
 }
